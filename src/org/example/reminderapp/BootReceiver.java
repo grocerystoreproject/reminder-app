e726fa25b703e8004b42ea7620d0d7778813550e
grocerystoreproject <85405446+grocerystoreproject@users.noreply.github.com>
2025-10-03 09:03:44 +0530

    Create BootReceiver.java

---
+++ src/org/example/reminderapp/BootReceiver.java
@@ -0,0 +1,15 @@
+package org.example.reminderapp;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class BootReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
+            Intent i = new Intent(context, AlarmReceiver.class);
+            context.sendBroadcast(i);
+        }
+    }
+}
